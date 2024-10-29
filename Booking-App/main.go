5df2c88776797d1e1f0d5108f00ecfedca0f468e
Revprm <revy.pramana@gmail.com>
2024-10-29 16:55:59 +0700

    feat: added up to switch statement

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -1,63 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main(){
-	// variables and Data Types
+func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-
-	// arrays and slices
 	bookings := []string{}
-	
-	// conferenceTickets = 100 // cannot assign to conferenceTickets as it is a constant
-	fmt.Printf("conferenceTickets is %T\nremainingTickets is %T\nconferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println	("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend.")
 
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
+		fmt.Println("Enter Your First Name: ")
+		fmt.Scanln(&firstName)
 
+		fmt.Println("Enter Your Last Name: ")
+		fmt.Scanln(&lastName)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+		fmt.Println("Enter Your Email: ")
+		fmt.Scanln(&email)
 
-	// ask user to enter their name
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter Numbers of Tickets: ")
+		fmt.Scanln(&userTickets)
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
-	
-	fmt.Println("Enter your Email Address")
-	fmt.Scan(&email)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-	fmt.Println("How many tickets you want to book?")
-	fmt.Scan(&userTickets)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-	remainingTickets = remainingTickets - userTickets
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 
-	// bookings[0] = firstName + " " + lastName
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
-	bookings = append(bookings, firstName + " " + lastName)
+			if remainingTickets == 0 {
+				// End program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you entered is too short.")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain @ sign.")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid.")
+			}
+		}
+	}
 
-	// fmt.Println(remainingTickets)
-	// fmt.Println(&remainingTickets)
+	city := "London"
 
-	// firstName = "Revy"
-	// userTickets = 2
-
-	fmt.Printf("The whole array is %v\n", bookings)
-	fmt.Printf("The first value is %v\n", bookings[0])
-	fmt.Printf("Slice type is %T\n", bookings)
-	fmt.Printf("The Slice length is %v\n", len(bookings))
-
-	fmt.Printf("Thank you %v %v for booking %v tickets for %v. You will receive a confirmation Email at %v\n", firstName, lastName, userTickets, conferenceName, email)
-	fmt.Printf("We have %v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-	fmt.Printf("These are all our bookings is %v\n", bookings)
+	switch city {
+	case "New York":
+		fmt.Println("New York is in America.")
+	case "London":
+		fmt.Println("London is in United Kingdom.")
+	case "Paris":
+		fmt.Println("Paris is in France.")
+		
+	default:
+		fmt.Println("City not found.")
+	}
 }
