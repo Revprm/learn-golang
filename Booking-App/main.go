2673165657470e344147084548018cde549e54ca
Revprm <revy.pramana@gmail.com>
2024-10-31 17:27:44 +0700

    feat: changed bookings to maps

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"Booking-App/helper"
+	"strconv"
+	// "strings"
 )
 
 const conferenceTickets int = 50
 
 var conferenceName = "Go Conference"
 var remainingTickets uint = 50
-var bookings = []string{}
+var bookings = make([]map[string]string, 0)
 
 func main() {
 	greetUsers()
@@ -18,7 +18,7 @@ func main() {
 	for {
 
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 			bookTicket(firstName, lastName, email, userTickets)
@@ -54,12 +54,10 @@ func greetUsers() {
 func getFirstnames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking["firstName"])
 	}
 	return firstNames
 }
-
 
 func getUserInput() (string, string, string, uint) {
 	var firstName string
@@ -84,8 +82,18 @@ func getUserInput() (string, string, string, uint) {
 
 func bookTicket(firstName string, lastName string, email string, userTickets uint) {
 	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
-
+	
+	// create a map for a user
+	var userData = make(map[string]string)
+	
+	userData["firstName"] = firstName
+	userData["lastName"] = lastName
+	userData["email"] = email
+	userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+	
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
+	
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 }
